8ddfd3e4004a7f8435ec7a7135f4d3e76a7169ae
Petr Chromec <mortal_flesh@seznam.cz>
2017-11-11 10:26:15 +0100

    Message formatter UT added and refactored

--- tests/AbstractTestCase.php
+++ tests/AbstractTestCase.php
@@ -7,6 +7,13 @@ use PHPUnit\Framework\TestCase;
 
 abstract class AbstractTestCase extends TestCase
 {
+    protected function expectsUnicode(string $withUnicode, string $withoutUnicode): string
+    {
+        return '\\' === DIRECTORY_SEPARATOR
+            ? $withoutUnicode
+            : $withUnicode;
+    }
+
     protected function tearDown()
     {
         m::close();
